c2f486b7316957e013363259293b9587d022b006
eleven <1720078416@qq.com>
2023-08-23 16:58:50 +0800

    feat: init

---
+++ apps/src/pages/Home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>欢迎来到首页！</h1>
+    <p>这是我们的新网站，希望你喜欢。</p>
+    <el-button @click="jumpTo(`\about`)">跳转到about</el-button>
+  </div>
+</template>
+
+<script setup name="Home" lang="ts">
+import useCommon from '@/hooks/useCommon';
+
+
+const { jumpTo } = useCommon();
+
+
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
